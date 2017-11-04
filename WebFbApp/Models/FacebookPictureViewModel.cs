4996321a7be7f6109360a811bbf2636e263f02ba
Stefan Jovic <jovic@live.com>
2017-11-04 19:44:05 +0100

    Picture Update

---
+++ WebFbApp/Models/FacebookPictureViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebFbApp.Models
+{
+
+    public class FacebookPictureViewModel
+    {
+        [Required]
+        [Display(Name = "Id")]
+        public string Id { get; set; }
+
+        [Display(Name = "Created_Time")]
+        public string CreatedTime { get; set; }
+
+        [Required]
+        [Display(Name = "PictureURL")]
+        public string PictureURL { get; set; }
+
+
+    }
+}
---
+++ WebFbApp/Models/FacebookWebhookViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebFbApp.Models
+{
+    public class FacebookWebhookViewModel
+    {
+        [Required]
+        [Display(Name = "Challenge ")]
+        public string Challenge { get; set; }
+    }
+}
